1a163b791433c4aeb349afa3e6ee15b1d8b59a94
job karani <job.nyambura@student.moringaschool.com>
2021-09-09 23:26:26 +0300

    init

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $("div#click-one").click(function(event) {
+      var whatToSay = "Hello!";
+      alert(whatToSay);
+    });
+
+    $("div#click-two").click(function(event) {
+      alert(whatToSay);
+    });
+  });
+
+  // here the variable is local
